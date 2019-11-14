fe63f8b9d94a80ee768431adeb45a381ab8f2369
Ghost.yang <Ghost.yang@skylinetw.com>
2019-11-14 11:11:37 +0800

    execute sp from C# and get data correctly.

---
+++ src/Demo.Data/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Data
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
